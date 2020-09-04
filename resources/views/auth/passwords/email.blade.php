4f10f4c2468f72fe833de7deaf93c14ce239549f
saadullah <saadch2020@gmail.com>
2020-09-04 19:11:38 +0500

    dont know what is going on

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,6 +8,8 @@
                 <div class="card-header">{{ __('Reset Password') }}</div>
 
                 <div class="card-body">
+
+
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
